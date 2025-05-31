51aba3078127a5283f9c823b2f9f8405486aa15f
Jacob Shogren <shogrenjacob@gmail.com>
2025-05-31 19:28:41 -0400

    added contact form handling

---
+++ src/scripts/ContactFormHandler.js
@@ -0,0 +1,15 @@
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        emailjs.sendForm('service_30x8d8g', 'template_6k6gsoq', this)
+            .then(() => {
+                console.log('SUCCESS!');
+                alert("Thank you for your response!");
+            }, (error) => {
+                console.log('FAILED...', error);
+                alert("ERROR")
+            });
+    });
+}
