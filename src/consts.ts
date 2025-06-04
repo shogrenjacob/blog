50a25fce68ff7c7c7db79a224fa17b13307bad8d
Jacob Shogren <shogrenjacob@gmail.com>
2025-06-04 16:14:58 -0400

    started feelin lucky button logic

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+import { getCollection } from 'astro:content';
+
 export const SITE_TITLE = "Jacob's Blog";
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const POST_LIST = (await getCollection('blog')).filter((post) => post.data.category === 'Computing').sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),);
