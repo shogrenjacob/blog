652feb1324cddf0d4fe041de268cf40d427543fc
Jacob Shogren <shogrenjacob@gmail.com>
2025-12-21 23:46:50 -0500

    Redid blog format, added photo gallery links

--- src/consts.ts
+++ src/consts.ts
@@ -5,5 +5,6 @@ import { getCollection } from 'astro:content';
 
 export const SITE_TITLE = "Jacob's Blog";
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const CATEGORIES = ["Media", "Sports", "Hobbies", "Computing", "Life"];
 
 export const POST_LIST = (await getCollection('blog')).filter((post) => post.data.category === 'Computing').sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),);
