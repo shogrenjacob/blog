728a4507d7c15679fb95e4b9233276594539b256
Jacob Shogren <shogrenjacob@gmail.com>
2026-01-04 23:44:24 -0500

    added color theme for code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://jacobshogren.blog',
     integrations: [mdx(), sitemap(), react()],
+    markdown: {
+        shikiConfig: {
+            theme: 'gruvbox-dark-medium'
+        },
+    },
 });
